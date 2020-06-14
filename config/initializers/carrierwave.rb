83dd10e75ea20c00925d7d22e8a243838d526ff4
yujiasano <forever2011.0903@gmail.com>
2020-06-14 21:42:07 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,11 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: AWS_ACCESS_KEY_ID,
-    aws_secret_access_key: AWS_SECRET_ACCESS_KEY,
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'timimage'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/timimage'
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'# S3バケット作成時に指定したリージョン。左記は東京を指す
+    }
+    config.fog_directory  = 'timimage' # 作成したS3バケット名
+  end
 end
